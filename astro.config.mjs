0d4f96d5c5639dcc10702670d712fca2675e65c2
Lemi Be <lemibekele365@gmail.com>
2026-04-15 16:29:19 +0300

    Switch adapter from vercel to node standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
+  adapter: node({
+    mode: 'standalone',
   }),
   vite: {
     plugins: [tailwindcss()],
